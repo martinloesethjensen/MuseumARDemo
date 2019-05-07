ebbb07d4a8bf82737c975f52e43aa748ea4aea84
Martin L. Jensen <martinloesethjensen@gmail.com>
2019-05-07 14:24:33 +0200

    Initial Commit

---
+++ MuseumAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MuseumAR
+//
+//  Created by Martin Løseth Jensen on 07/05/2019.
+//  Copyright © 2019 Martin Løseth Jensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
