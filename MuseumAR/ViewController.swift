96ea38ef39a8baaf1cc5db14d30c404227753236
Martin L. Jensen <martinloesethjensen@gmail.com>
2019-05-10 11:50:59 +0200

    Added code and files that were missing

--- MuseumAR/ViewController.swift
+++ MuseumAR/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ARSKViewDelegate{
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
+    @IBOutlet weak var arskView: ARSKView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        arskView.delegate = self
+        arskView.showsFPS = true
+        if let scene = SKScene(fileNamed: "MyScene"){
+            arskView.presentScene(scene)
+        }
+        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil)
+            else {
+                fatalError("no image found for ARWoldTrackingConfiguration")
+        }
+        
+        configuration.detectionImages = referenceImages
+        arskView.session.run(configuration)
     }
-
-
+    
+    @IBAction func returned(segue: UIStoryboardSegue) {}
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        arskView.session.run(configuration)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        arskView.session.pause()
+        
+    }
+    
+    func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
+        if let imageAnchor = anchor as? ARImageAnchor, let referenceImageName = imageAnchor.referenceImage.name {
+            print("found image named: \(referenceImageName)")
+            performSegue(withIdentifier: "segue1", sender: self)
+            
+        }
+        return nil
+    }
+    
 }
-
